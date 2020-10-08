eb10cff6c61bc119a2d96d61abb5ad060380e4eb
osamudiamen <osas.azamegbe@gmail.com>
2020-10-08 13:33:48 +0100

    created mongodb

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 
 const app = express()
@@ -7,6 +9,21 @@ const PORT = process.env.PORT || 5000
 //ROUTES
 app.get('/', (req, res) =>{
     res.send('Welcome to the home route')
+})
+
+
+//DB
+const DB_NAME = process.env.DB_NAME
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.yttoy.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},
+() =>{
+    console.log('connected to database!')
 })
 
 //SERVER LISTEN
