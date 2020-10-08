9e5f13739301fa37638af039411bcac650463b94
osamudiamen <osas.azamegbe@gmail.com>
2020-10-08 14:17:54 +0100

    created post model and schema

---
+++ models/Post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+postModel = mongoose.model('Posts', postSchema)
+
+module.exports = postModel
